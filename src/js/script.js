cef28ff68bab549e99a1095985bd9fb5d97c86b7
elewis9989 <elewis9989@gmail.com>
2017-07-25 16:36:11 -0700

    added favicon

--- src/js/script.js
+++ src/js/script.js
@@ -21,28 +21,41 @@ $(document).ready(function(){
         .tiSettings({speed: 100})
         .tiType('...');
 
-        // Get the modal
-        var modal = document.getElementById('about');
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
+        
 
 });
+
+// Get the modal
+var modal = document.getElementById('about');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+// Overlay
+var overlay = document.getElementById("overlay");
+
+// Event Listener
+window.addEventListener('load', function(){
+    overlay.style.display = 'none';
+});
+
+
